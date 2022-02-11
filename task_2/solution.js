ffdb3b6ef91b29df8056cec6ed93731900d54ebe
Danila Mazein <dan.mazein@yandex.ru>
2022-02-11 14:16:46 +0300

    task 2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,6 +6,12 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
+    let shippingSum
+    if (productsSum == 0 || productsSum >= freeShippingMinSum){
+        shippingSum = 0;
+    } else if  (productsSum > 0 && productsSum < freeShippingMinSum){
+        shippingSum = shippingPrice;
+    }
 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
@@ -29,7 +35,10 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
-
+    let discountSum;
+    if (productsSum >= discountMinSum){
+        discountSum = discountPart * productsSum / 100;
+    } else discountSum = 0;
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
@@ -44,21 +53,19 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
-
     // создайте переменную totalSum
-
+    let totalSum = productsSum - discountSum;
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
     // прибавьте к totalSum значение shippingSum
-
+    totalSum += shippingSum;
+    let freeShipping = shippingSum == 0;
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
-
     // Конец решения задачи №2.3.
-
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
 }
